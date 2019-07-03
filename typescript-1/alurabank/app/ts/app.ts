aaa4b493e15624b5139fca4b60f387d24087b8fd
Vinicius Martins <vinibitmartins@gmail.com>
2019-07-02 23:30:08 -0300

    Conclusão da aula 02.

--- typescript-1/alurabank/app/ts/app.ts
+++ typescript-1/alurabank/app/ts/app.ts
@@ -1,4 +1,6 @@
 // app/ts/app.js
 
-let negociacao = new Negociacao(new Date(), 2, 100);
-console.log(negociacao);
+const controller = new NegociacaoController();
+document
+    .querySelector('.form')
+    .addEventListener('submit',controller.adiciona.bind(controller));
--- typescript-1/alurabank/app/ts/models/Negociacao.ts
+++ typescript-1/alurabank/app/ts/models/Negociacao.ts
@@ -2,31 +2,21 @@
 
 class Negociacao {
 
-    private _data;
-    private _quantidade;
-    private _valor;
-
-    constructor(data, quantidade, valor) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    }
+    constructor(private _data: Date, private _quantidade: number, private _valor: number) {}
 
     get data() {
 
-        return this.data;
-
+        return this._data;
     }
 
     get quantidade() {
 
-        return this.quantidade;
-
+        return this._quantidade;
     }
 
     get valor() {
 
-        return this.valor;
+        return this._valor;
     }
 
     get volume() {
