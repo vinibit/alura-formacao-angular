be63d73a6a4c85aaba51c8a051158aa2a657c24c
Vinicius Martins <vinibitmartins@gmail.com>
2020-07-21 19:32:38 -0300

    Aula 05: Importando scripts

--- webpack/projeto-webpack/client/app-src/app.js
+++ webpack/projeto-webpack/client/app-src/app.js
@@ -1,8 +1,12 @@
-import { NegociacaoController } from './controllers/NegociacaoController.js';
-import { Negociacao } from './domain/index.js';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'bootstrap/js/modal.js';
 import '../css/meucss.css';
+import { NegociacaoController } from './controllers/NegociacaoController.js';
+import { Negociacao } from './domain/index.js';
+
+//$('h1').click(() => alert('Você me clicou!'));
+//console.log($('#mensagemView').modal);
 
 const controller = new NegociacaoController();
 const negociacao = new Negociacao(new Date(), 1, 200);
