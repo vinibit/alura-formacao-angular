2ce5e6ac91c34288079825d47022448c5f237b82
Vinicius Martins <vinibitmartins@gmail.com>
2020-10-02 18:16:37 -0300

    Aula 01: Criação do projeto alurapic

---
+++ angular-1/alurapic/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Alura.pic';
+  description = 'Linda';
+  url = 'https://media-exp1.licdn.com/dms/image/C4D03AQH8WRb5jRRmBg/profile-displayphoto-shrink_200_200/0?e=1606953600&v=beta&t=4Mp9I1bkYKskeqCqPZosmWLf09pGuk6fnEy2lx93vSU';
+}
