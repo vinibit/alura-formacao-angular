f51ea22a990a0b6a36acf6182c9ba3ff6bd2b677
Vinicius Martins <vinibitmartins@gmail.com>
2020-10-05 19:54:47 -0300

    Aula 02: Criando o primeiro componente.

--- angular-1/alurapic/src/app/app.component.ts
+++ angular-1/alurapic/src/app/app.component.ts
@@ -7,6 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Alura.pic';
-  description = 'Linda';
-  url = 'https://media-exp1.licdn.com/dms/image/C4D03AQH8WRb5jRRmBg/profile-displayphoto-shrink_200_200/0?e=1606953600&v=beta&t=4Mp9I1bkYKskeqCqPZosmWLf09pGuk6fnEy2lx93vSU';
+
+  photos = [
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg',
+      description: 'Leão'
+    },
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg',
+      description: 'Leoa'
+    },
+    {
+      url: 'https://media-exp1.licdn.com/dms/image/C4D03AQH8WRb5jRRmBg/profile-displayphoto-shrink_200_200/0?e=1606953600&v=beta&t=4Mp9I1bkYKskeqCqPZosmWLf09pGuk6fnEy2lx93vSU',
+      description: 'Linda'
+    }
+  ];
 }
