d7761abc904bf92bde94dc87ae6984a520174ae1
Vinicius Martins <vinibitmartins@gmail.com>
2020-10-07 12:01:42 -0300

    Aula 03: Integração com Web APIS's

--- angular-1/alurapic/src/app/app.component.ts
+++ angular-1/alurapic/src/app/app.component.ts
@@ -1,25 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from './photos/photo/photo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Alura.pic';
+export class AppComponent implements OnInit {
 
-  photos = [
-    {
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg',
-      description: 'Leão'
-    },
-    {
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg',
-      description: 'Leoa'
-    },
-    {
-      url: 'https://media-exp1.licdn.com/dms/image/C4D03AQH8WRb5jRRmBg/profile-displayphoto-shrink_200_200/0?e=1606953600&v=beta&t=4Mp9I1bkYKskeqCqPZosmWLf09pGuk6fnEy2lx93vSU',
-      description: 'Linda'
-    }
-  ];
+  title = 'Alura.pic';
+  photos: Object[] = []
+  
+  constructor(private photoService: PhotoService) { }
+
+  ngOnInit(): void {
+    this.photoService
+        .listFromUser('flavio')
+        .subscribe(photos =>  {
+          this.photos = photos;
+      });
+  }
+
 }
