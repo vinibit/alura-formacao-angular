6521ef71a5a8476d6f9b18dc80ccf325c3d8b703
Vinicius Martins <vinibitmartins@gmail.com>
2020-10-09 19:29:10 -0300

    Aula 04: Single Page Applications e rotas

--- angular-1/alurapic/src/app/photos/photos.module.ts
+++ angular-1/alurapic/src/app/photos/photos.module.ts
@@ -1,10 +1,20 @@
-import { HttpClientModule } from "@angular/common/http";
-import { NgModule } from "@angular/core";
-import { PhotoComponent } from "./photo/photo.component";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+import { PhotoComponent } from './photo/photo.component';
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
 @NgModule({
-    declarations: [ PhotoComponent ],
-    exports: [ PhotoComponent ],
-    imports: [ HttpClientModule ]
+    declarations: [ 
+        PhotoComponent, 
+        PhotoListComponent, 
+        PhotoFormComponent 
+    ],
+    imports: [
+        HttpClientModule ,
+        CommonModule
+    ]
 })
 export class PhotosModule {}
