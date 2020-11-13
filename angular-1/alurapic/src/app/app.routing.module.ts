1c8fa26371ced1fe40f6f9970c8103d0f20da437
Vinicius Martins <vinibitmartins@gmail.com>
2020-11-13 18:52:41 -0300

    Aula 06: Melhorando a experiênica do usuário

--- angular-1/alurapic/src/app/app.routing.module.ts
+++ angular-1/alurapic/src/app/app.routing.module.ts
@@ -4,9 +4,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { resolve } from 'url';
+import { PhotoListresolver } from './photos/photo-list/photo-list.resolver';
 
 const routes: Routes = [
-    { path: 'user/:userName', component: PhotoListComponent },
+    { 
+        path: 'user/:userName', 
+        component: PhotoListComponent,
+        resolve: {
+            photos: PhotoListresolver
+        }
+    },
     { path: 'p/add', component: PhotoFormComponent },
     { path: '**', component: NotFoundComponent }
 ];
