5801420523a42496005780505828b17fd8899fb2
Vini Martins <vinibitmartins@gmail.com>
2021-05-21 18:03:47 -0300

    Aula 06: Component de notificação

--- angular-3/alurapic/src/app/photos/photo-form/photo-form.component.ts
+++ angular-3/alurapic/src/app/photos/photo-form/photo-form.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { PhotoService } from '../photo/photo.service';
+import { AlertService } from '../../shared/components/alert/alert.service';
+import { UserService } from '../../core/user/user.service';
 
 @Component({
     selector: 'ap-photo-form',
@@ -16,7 +18,9 @@ export class PhotoFormComponent implements OnInit {
     constructor(
         private formBuilder: FormBuilder,
         private photoService: PhotoService,
-        private router: Router) { }
+        private router: Router,
+        private alertService: AlertService,
+        private userService: UserService) { }
 
     ngOnInit() {
         this.photoForm = this.formBuilder.group({
@@ -31,7 +35,10 @@ export class PhotoFormComponent implements OnInit {
         const allowComments  = this.photoForm.get('allowComments').value;
         this.photoService
             .upload(description, allowComments, this.file)
-            .subscribe(() => this.router.navigate(['']));
+            .subscribe(() => {
+                this.alertService.success('Upload completed.', true);
+                this.router.navigate(['/user', this.userService.getUserName()]);
+            }); 
     }
 
     handleFile(file: File) {
