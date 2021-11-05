6f237734e193ee12f08c588b7fa73f2a27c0a235
Vini Martins <vinibitmartins@gmail.com>
2021-11-05 17:47:01 -0300

    Aula 07: Um pouco mais sobre navegação e rotas

--- angular-4/alurapic/src/app/home/signin/signin.component.ts
+++ angular-4/alurapic/src/app/home/signin/signin.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from '../../core/auth/auth.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PlatformDetectorService } from '../../core/plataform-detector/platform-detector.service';
 
 @Component({
@@ -9,6 +9,7 @@ import { PlatformDetectorService } from '../../core/plataform-detector/platform-
 })
 export class SignInComponent implements OnInit {
     
+    fromUrl: string
     loginForm: FormGroup;
     @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>;
     
@@ -16,9 +17,13 @@ export class SignInComponent implements OnInit {
         private formBuilder: FormBuilder,
         private authService: AuthService,
         private router: Router,
-        private platformDetectorService: PlatformDetectorService) { }
+        private platformDetectorService: PlatformDetectorService,
+        private activatedRoute: ActivatedRoute) { }
 
     ngOnInit(): void {
+        this.activatedRoute
+            .queryParams
+            .subscribe(params => this.fromUrl = params['fromUrl']);
         this.loginForm = this.formBuilder.group({
             userName: ['', Validators.required],
             password: ['', Validators.required]
@@ -34,7 +39,11 @@ export class SignInComponent implements OnInit {
         this.authService
             .authenticate(userName, password)
             .subscribe(
-                () => this.router.navigate(['user', userName]),
+                () => { 
+                    this.fromUrl
+                        ? this.router.navigateByUrl(this.fromUrl)
+                        : this.router.navigate(['user', userName]);
+                },
                 err => {
                     console.log(err);
                     this.loginForm.reset();
